ad3e2c41b022f8b0c623aa5c9a308c7704d10791
Wycliffe <wycliffekiprono4@gmail.com>
2023-04-23 09:54:58 +0300

    add useEffect

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
-
+  
   // add useEffect code
+  //Set a timer using setTimeout that decrements the timeRemaining state by 1 every second
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime - 1)
+    }, 1000)
+    return () => clearTimeout(timer) //Return a cleanup function that clears the timer using clearTimeout to avoid memory leaks 
+  }, [timeRemaining])
+  
+  // Check if timeRemaining has reached 0, if true: reset timeRemaining to 10 and call onAnswered with a value of false
+  useEffect (() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
